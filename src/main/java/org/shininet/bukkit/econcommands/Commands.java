a86e585d1173f9853be9650ffedcc0bf63499733
meiskam <meiskam@gmail.com>
2015-01-12 20:35:18 -0500

    Add WorldEdit depend, add balance command

---
+++ src/main/java/org/shininet/bukkit/econcommands/Commands.java
@@ -0,0 +1,62 @@
+package org.shininet.bukkit.econcommands;
+
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissionsException;
+import com.sk89q.minecraft.util.commands.NestedCommand;
+
+public class Commands {
+
+	public static class Parent {
+		@Command(aliases = {"EconCommands", "money"}, desc = "Economy commands")
+		@NestedCommand({Commands.class})
+		public static void money() {
+		}
+	}
+
+	private static void requirePermission(CommandSender sender, String permission) throws CommandPermissionsException {
+		if (!(EconCommands.inst().commands.hasPermission(sender, permission))) {
+			throw new CommandPermissionsException();
+		}
+	}
+
+	private static Economy econ() {
+		return EconCommands.econ;
+	}
+
+	@SuppressWarnings("deprecation")
+	private static OfflinePlayer offlinePlayer(String name) {
+		return Bukkit.getOfflinePlayer(name);
+	}
+
+	@Command(aliases = {"balance"}, usage = "[player]", desc = "Check a player balance", min = 0, max = 1)
+	public static void balance(CommandContext args, CommandSender sender) throws CommandException {
+		double balance;
+		if (args.argsLength() == 0) {
+			requirePermission(sender, "econcommands.balance.self");
+			if (!(sender instanceof Player)) {
+				throw new CommandException("Command must either be issued by a player or have player name in arguments");
+			}
+			Player player = (Player) sender;
+			balance = econ().getBalance(player);
+			player.sendMessage("Balance: " + econ().format(balance) + " " + ((balance == 1)?econ().currencyNameSingular():econ().currencyNamePlural()));
+		} else {
+			requirePermission(sender, "econcommands.balance.other");
+			OfflinePlayer playerOther = offlinePlayer(args.getString(0));
+			if (!(econ().hasAccount(playerOther))) {
+				sender.sendMessage("That player does not have an account");
+				return;
+			}
+			balance = econ().getBalance(playerOther);
+		}
+		sender.sendMessage("Balance: " + econ().format(balance) + " " + ((balance == 1)?econ().currencyNameSingular():econ().currencyNamePlural()));
+	}
+}
--- src/main/java/org/shininet/bukkit/econcommands/EconCommands.java
+++ src/main/java/org/shininet/bukkit/econcommands/EconCommands.java
@@ -1,7 +1,102 @@
 package org.shininet.bukkit.econcommands;
 
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.bukkit.util.CommandsManagerRegistration;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissionsException;
+import com.sk89q.minecraft.util.commands.CommandUsageException;
+import com.sk89q.minecraft.util.commands.CommandsManager;
+import com.sk89q.minecraft.util.commands.MissingNestedCommandException;
+import com.sk89q.minecraft.util.commands.WrappedCommandException;
 
 public class EconCommands extends JavaPlugin {
 
+	private static EconCommands instance;
+
+	CommandsManager<CommandSender> commands;
+	static Economy econ;
+
+	public EconCommands() {
+		super();
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	static EconCommands inst() {
+		return instance;
+	}
+
+	public void onEnable() {
+		if (!setupEconomy()) {
+			die("Disabled due to no Vault dependency found!");
+			return;
+		}
+		try {
+			Class.forName("com.sk89q.minecraft.util.commands.CommandsManager");
+		} catch (ClassNotFoundException e) {
+			die("Disabled due to no WorldEdit dependency found!");
+			return;
+		}
+
+		// Register commands with sk89q's manager
+		commands = new CommandsManager<CommandSender>() {
+			@Override
+			public boolean hasPermission(CommandSender player, String perm) {
+				return player.hasPermission(perm);
+			}
+		};
+
+		final CommandsManagerRegistration cmdRegister = new CommandsManagerRegistration(this, commands);
+		cmdRegister.register(Commands.Parent.class);
+	}
+
+	private void die(String message) {
+		getLogger().severe(message);
+		getServer().getPluginManager().disablePlugin(this);
+	}
+
+	private boolean setupEconomy() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = rsp.getProvider();
+		return econ != null;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		try {
+			commands.execute(cmd.getName(), args, sender, sender);
+		} catch (CommandPermissionsException e) {
+			sender.sendMessage(ChatColor.RED + "You don't have permission.");
+		} catch (MissingNestedCommandException e) {
+			sender.sendMessage(ChatColor.RED + e.getUsage());
+		} catch (CommandUsageException e) {
+			sender.sendMessage(ChatColor.RED + e.getMessage());
+			sender.sendMessage(ChatColor.RED + e.getUsage());
+		} catch (WrappedCommandException e) {
+			if (e.getCause() instanceof NumberFormatException) {
+				sender.sendMessage(ChatColor.RED + "Number expected, string received instead.");
+			} else {
+				sender.sendMessage(ChatColor.RED + "An error has occurred. See console.");
+				e.printStackTrace();
+			}
+		} catch (CommandException e) {
+			sender.sendMessage(ChatColor.RED + e.getMessage());
+		}
+
+		return true;
+	}
 }
