bd40f3203a42780f983e5607af2e0086889e2296
meiskam <meiskam@gmail.com>
2015-02-21 04:58:22 -0500

    Added admin command set

--- src/main/java/org/shininet/bukkit/econcommands/CommandsMoneyAdmin.java
+++ src/main/java/org/shininet/bukkit/econcommands/CommandsMoneyAdmin.java
@@ -54,4 +54,26 @@ public class CommandsMoneyAdmin {
 			sender.sendMessage("Error, could not remove from "+playerOther.getName()+"'s account");
 		}
 	}
+
+	@Command(aliases = {"set"}, usage = "<player> <amount>", desc = "Set the currency of a player's account", min = 2, max = 2)
+	public static void set(CommandContext args, CommandSender sender) throws CommandException {
+		EconCommands.requirePermission(sender, "econcommands.admin.set");
+		OfflinePlayer playerOther = EconCommands.offlinePlayer(args.getString(0));
+		if (!(econ().hasAccount(playerOther))) {
+			sender.sendMessage("That player does not have an account");
+			return;
+		}
+		Double amount = args.getDouble(1);
+		if (amount < 0) {
+			sender.sendMessage("Amount must be at least 0");
+			return;
+		}
+		econ().withdrawPlayer(playerOther, econ().getBalance(playerOther));
+		
+		if (econ().depositPlayer(playerOther, amount).transactionSuccess()) {
+			sender.sendMessage("Set "+playerOther.getName()+"'s account, new balance: "+econ().getBalance(playerOther));
+		} else {
+			sender.sendMessage("Error, could not set "+playerOther.getName()+"'s account");
+		}
+	}
 }
