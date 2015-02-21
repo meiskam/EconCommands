293a914b192e2c0c1674fe93bd99714d725c05f5
meiskam <meiskam@gmail.com>
2015-02-21 04:50:40 -0500

    Added admin command remove

--- src/main/java/org/shininet/bukkit/econcommands/CommandsMoneyAdmin.java
+++ src/main/java/org/shininet/bukkit/econcommands/CommandsMoneyAdmin.java
@@ -34,4 +34,24 @@ public class CommandsMoneyAdmin {
 			sender.sendMessage("Error, could not add to "+playerOther.getName()+"'s account");
 		}
 	}
+
+	@Command(aliases = {"remove"}, usage = "<player> <amount>", desc = "Remove currency from player's account", min = 2, max = 2)
+	public static void remove(CommandContext args, CommandSender sender) throws CommandException {
+		EconCommands.requirePermission(sender, "econcommands.admin.remove");
+		OfflinePlayer playerOther = EconCommands.offlinePlayer(args.getString(0));
+		if (!(econ().hasAccount(playerOther))) {
+			sender.sendMessage("That player does not have an account");
+			return;
+		}
+		Double amount = args.getDouble(1);
+		if (amount <= 0) {
+			sender.sendMessage("Amount must be greater than 0");
+			return;
+		}
+		if (econ().withdrawPlayer(playerOther, amount).transactionSuccess()) {
+			sender.sendMessage("Removed "+EconCommands.format(amount)+" from "+playerOther.getName()+"'s account, new balance: "+econ().getBalance(playerOther));
+		} else {
+			sender.sendMessage("Error, could not remove from "+playerOther.getName()+"'s account");
+		}
+	}
 }
